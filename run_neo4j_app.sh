e5dd3b4846e8d7f880d32ebc0e212d81dab2c800
Kaustav Mukherjee <oracler4284@gmail.com>
2023-01-18 00:47:20 +0530

    Update run_neo4j_app.sh

--- run_neo4j_app.sh
+++ run_neo4j_app.sh
@@ -6,6 +6,6 @@ sudo chmod -R 777 HOME/neo4j/import
 docker run -d -it --rm   --publish=7474:7474 --publish=7687:7687   --user="$(id -u):$(id -g)" -v $HOME/neo4j/import:/var/lib/neo4j/import  -e NEO4J_AUTH=none   --env NEO4JLABS_PLUGINS='["apoc","graph-data-science"]'   neo4j:5.2.0
 
 
-##Run Neo-Dash Docker:
+## Run Neo-Dash Docker:
 
 docker run -d -it --rm -p 5005:5005 nielsdejong/neodash
